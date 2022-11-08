90804e7aacf664b20cdfd00f422e57b6424fcac9
Gabriel Sitorus <sitorusgg@gmail.com>
2022-11-08 16:05:09 +0700

    restart spotify auth

--- client/src/components/Authentication/Login/Login.js
+++ client/src/components/Authentication/Login/Login.js
@@ -9,22 +9,6 @@ function Login() {
     const goHome = () => navigate('/')
     const goSignup = () => navigate('/signup')
 
-    const [token, setToken] = useState('')
-
-    useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem('token')
-
-        console.log(token)
-
-        if (token && hash) {
-            token = hash.substring(1).split('&').find(el => el.startsWith('access_token')).split('=')(1)
-
-            window.location.hash = '';
-            window.localStorage.setItem('token', token)
-            setToken(token)
-        }
-    }, [])
 
 
     return (
